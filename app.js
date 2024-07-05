9bd3a0ada15b1159504a1b344af7c28056d1f812
Atharv Kulkarni <atharvkulkarni429@gmail.com>
2024-07-05 22:13:57 +0530

    cryptojs

--- app.js
+++ app.js
@@ -11,11 +11,10 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 const findOrCreatePlugin = require("mongoose-findorcreate");
-const crypto = require("crypto");
+const CryptoJS = require("crypto-js");
 
 const app = express();
-const algorithm = "aes-256-cbc";
-const key = crypto.scryptSync(process.env.SECRET, "salt", 32);
+const secretKey = process.env.SECRET_KEY;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -33,21 +32,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 function encrypt(text) {
-  const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv(algorithm, key, iv);
-  let encrypted = cipher.update(text, "utf8", "hex");
-  encrypted += cipher.final("hex");
-  return iv.toString("hex") + ":" + encrypted;
+    return CryptoJS.AES.encrypt(text, secretKey).toString();
 }
 
-function decrypt(text) {
-  const textParts = text.split(":");
-  const iv = Buffer.from(textParts.shift(), "hex");
-  const encryptedText = Buffer.from(textParts.join(":"), "hex");
-  const decipher = crypto.createDecipheriv(algorithm, key, iv);
-  let decrypted = decipher.update(encryptedText, "hex", "utf8");
-  decrypted += decipher.final("utf8");
-  return decrypted;
+function decrypt(ciphertext) {
+    const bytes = CryptoJS.AES.decrypt(ciphertext, secretKey);
+    return bytes.toString(CryptoJS.enc.Utf8);
 }
 
 mongoose
